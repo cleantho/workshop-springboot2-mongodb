c1f05ba56fe03d5f346eb1a19bfa37411e1e866f
cleantho <cleantho@gmail.com>
2022-11-06 15:06:13 -0300

    Atualização de usuário com PUT

--- src/main/java/com/course/workshopmongo/services/UserService.java
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -27,14 +27,21 @@ public class UserService {
 	}
 
 	public User insert(User obj) {
-		return repo.insert(obj);		
+		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User origin = findById(obj.getId());
+		origin.setName(obj.getName());
+		origin.setEmail(obj.getEmail());
+		return repo.save(origin);
+	}
+
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
